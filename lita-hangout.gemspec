93e5e7db2503e449527c01892cb90bc2598a8427
Gabriel Mazetto <brodock@gmail.com>
2015-09-01 09:12:50 -0300

    Trying to test slack specific code

--- lita-hangout.gemspec
+++ lita-hangout.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'coveralls'
   spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'lita-slack', '~> 1.6'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ SimpleCov.start { add_filter '/spec/' }
 
 require 'lita-hangout'
 require 'lita/rspec'
+require 'lita-slack'
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
